b8fbb960af958616bb8cd3d42276a7e147d8fb1b
ayannw <ayanispro420@gmail.com>
2021-03-24 19:54:03 +0600

    display date and time

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,39 @@
-import Vue from '../node_modules/vue/dist/vue.esm.browser.min.js'
+import Vue from '../node_modules/vue/dist/vue.esm.browser.min.js';
 
-const r = new Vue({
-  el: '#body',
+const clock = new Vue({
+  el: '#clock',
   data: {
-    urmom: 'gay',
+    time: '',
+    date: '',
   },
-})
+})!
+
+const zeroPadding = (num: number, digit: number) => {
+  let zero: string = '';
+  for (let i = 0; i < digit; i++) {
+    zero += '0';
+  }
+  return (zero + num).slice(-digit);
+}
+
+const updateTime = () => {
+  const cd = new Date()
+  clock.time =
+    zeroPadding(cd.getHours(), 2) +
+    ':' +
+    zeroPadding(cd.getMinutes(), 2) +
+    ':' +
+    zeroPadding(cd.getSeconds(), 2)
+  clock.date =
+    zeroPadding(cd.getFullYear(), 4) +
+    '-' +
+    zeroPadding(cd.getMonth() + 1, 2) +
+    '-' +
+    zeroPadding(cd.getDate(), 2) +
+    ' ' +
+    week[cd.getDay()];
+}
+
+const week = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
+const timerID = setInterval(updateTime, 1000);
+updateTime();
